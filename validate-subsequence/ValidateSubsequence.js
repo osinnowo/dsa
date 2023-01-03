c0f336a1161fbe5dd8aae1cb4189d19319f4b921
Osinnowo Emmanuel <eosinnowo@getcarbon.co>
2023-01-03 18:55:38 +0100

    Validate Subsequence - Javascript

--- validate-subsequence/ValidateSubsequence.js
+++ validate-subsequence/ValidateSubsequence.js
@@ -1,3 +1,41 @@
-function validateSubsequence() {
-    
-}
+function isValidSubsequence1(array, sequence) {
+    let count = 0;
+    for (let index = 0; index < array.length; index++) {
+        if(sequence.length === count) break;
+        if(array[index] === sequence[count]) { count++; }
+    }
+    return sequence.length == count;
+  }
+
+  function isValidSubsequence2(array, sequence) {
+    let count = sequence.length - 1;
+    for(let index = 0; index < array.length; index++) {
+        if(count === -1) break;
+        if(array[array.length - 1 - index ] === sequence[count]) {
+            count--;
+        }
+    }
+    return count === -1;
+  }
+
+  function isValidSubsequence3(array, sequence) {
+    for(let index = 0; index < array.length; index++) {
+        if(sequence.length === 0) break
+        if(array[array.length - 1 - index ] === sequence[sequence.length - 1]) {
+           sequence.pop();
+        }
+    }
+    return sequence.length === 0;
+  }
+
+  function isValidSubsequence4(array, sequence) {
+    for(let index = 0; index < array.length; index++) {
+        if(sequence.length === 0) break
+        if(array[index] === sequence[0]) {
+           sequence.shift();
+        }
+    }
+    return sequence.length === 0;
+  }
+
+  exports.isValidSubsequence = isValidSubsequence;
