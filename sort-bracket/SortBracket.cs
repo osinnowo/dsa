74349c2c103abf03b91f9308a91046fc483a36e8
Osinnowo Emmanuel <eosinnowo@getcarbon.co>
2023-01-05 00:22:12 +0100

    Added Csharp Method

--- sort-bracket/SortBracket.cs
+++ sort-bracket/SortBracket.cs
@@ -15,9 +15,22 @@ public class SortBracket {
     }
 
     public static string SortedBracket(string str) {
-        int index = 0;
-        while(index < str.Length) {
-            
+        char[] characters = str.ToCharArray();
+        int left = 0;
+        int right = characters.Length - 1;
+        while(left < right) {
+            if(characters[left] == '(') { left++; continue; }
+            while(left < right) {
+                if(characters[right] == '(') {
+                    char temp = characters[right];
+                    characters[right] = characters[left];
+                    characters[left] = temp;
+                    left++; right--;
+                    break;
+                }
+                right--;
+            }
         }
+        return new String(characters);
     }
 }
