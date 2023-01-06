08cf5c2e58baf542d09a810f64b683309dfbd8b1
Osinnowo Emmanuel <eosinnowo@getcarbon.co>
2023-01-06 11:26:31 +0100

    NonDuplicate -> C# Solution

---
+++ non-duplicate-array/NonDuplicateArray.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+
+public class NonDuplicateArray {
+    public static int[] NonDuplicate(int[] array) {
+        Array.Sort(array);
+        List<int> results = new List<int>();
+        for (int i = 0; i < array.Length; i++)
+        {
+            int distinctIndex = i;
+            for (int j = i + 1; j < array.Length; j++)
+            {
+                if (array[distinctIndex] == array[j])
+                {
+                    distinctIndex = j;
+                }
+            }
+            results.Add(array[distinctIndex]);
+            i = distinctIndex;
+        }
+        return results.ToArray();
+    }
+
+    public static int[] NonDuplicate2(int[] array) {
+        HashSet<int> set = new HashSet<int>();
+        int index = 0;
+        while(index < array.Length) {
+            set.Add(array[index]);
+            index++;
+        }
+        return set.ToArray();
+    }
+
+    public static int[] NonDuplicate3(int[] array) {
+        HashSet<int> set = new HashSet<int>(); return set.ToArray();
+    }
+}
