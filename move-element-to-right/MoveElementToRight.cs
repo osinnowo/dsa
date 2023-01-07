1ddf26df9d694e49ac22d2b574b396403bcc5e2a
Osinnowo Emmanuel <eosinnowo@getcarbon.co>
2023-01-07 21:45:04 +0100

    Csharp Solution

---
+++ move-element-to-right/MoveElementToRight.cs
@@ -0,0 +1,41 @@
+using System;
+
+public class MoveElementToRight 
+{
+    public static int[] moveElementToRight(int[] array, int elementToMove) {
+        int left = 0;
+        int right = array.Length - 1;
+        while (left < right)
+        {
+            if (array[left] != elementToMove) { left++; continue; }
+            while (left < right)
+            {
+                if (array[right] != elementToMove)
+                {
+                    int temp = array[left];
+                    array[left] = array[right];
+                    array[right] = temp;
+                    left++; right--;
+                    break;
+                }
+                right--;
+            }
+        }
+        return array;
+    }
+
+    public static int[] moveElementToRight2(int[] array, int elementToMove)
+    {
+        int left = 0;
+        int right = array.Length - 1;
+        while(left < right) {
+            while(left < right && array[left] != elementToMove) { left++; }
+            while(left < right && array[right] == elementToMove) { right--; }
+            int temp = array[left];
+            array[left] = array[right];
+            array[right] = temp;
+            left++; right--;
+        }
+        return array;
+    }
+}
