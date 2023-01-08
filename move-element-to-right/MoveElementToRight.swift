33698404e1ed86bce7a07043692bc2d1e654dc9f
Osinnowo Emmanuel <eosinnowo@getcarbon.co>
2023-01-08 08:43:06 +0100

    Swift Solution

---
+++ move-element-to-right/MoveElementToRight.swift
@@ -0,0 +1,32 @@
+import Foundation
+
+class MoveElementToRight {
+    func moveElementToRight(array: inout [Int], elementToMove: Int) -> [Int] {
+        var left = 0
+        var right = array.count - 1
+        while(left < right) {
+            if(array[left] != elementToMove) { left += 1; continue; }
+            while(left < right) {
+                if(array[right] != elementToMove) {
+                    (array[left], array[right]) = (array[right], array[left])
+                    left += 1; right -= 1;
+                    break
+                }
+                right -= 1
+            }
+        }
+        return array
+    }
+
+    func moveElementToRight2(array: inout [Int], elementToMove: Int) -> [Int] {
+        var left = 0
+        var right = 0
+        while(left < right) {
+            while(left < right && array[left] != elementToMove) { left += 1 }
+            while(left < right && array[right] == elementToMove) { right -= 1}
+            array.swapAt(left, right)
+            left += 1; right -= 1;
+        }
+        return array
+    }
+}
