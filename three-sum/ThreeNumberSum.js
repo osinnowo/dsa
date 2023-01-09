aa506258dfb88350bd3499fb5e2ec0730958d9e3
Osinnowo Emmanuel <eosinnowo@getcarbon.co>
2023-01-09 19:26:17 +0100

    ThreeNumberSum -> Javascript Solution

--- three-sum/ThreeNumberSum.js
+++ three-sum/ThreeNumberSum.js
@@ -1,5 +1,22 @@
 function threeNumberSum(array, targetSum) {
-
+    array.sort((a, b) => a - b);
+    const results = [];
+    for(let index = 0; index < array.length; index++) {
+        let left = index + 1;
+        let right = array.length - 1;
+        while(left < right) {
+            let sum = array[index] + array[left] + array[right];
+            if(sum === targetSum) {
+                results.push([ array[index], array[left], array[right] ]);
+                left++; right--;
+            } else if (sum < targetSum) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+    }
+    return results;
 }
 
-exports.threeNumberSum = this.threeNumberSum;
+exports.threeNumberSum = threeNumberSum;
