4a1ee56091260a91087a9c835d15cd36df1ef1c3
Osinnowo Emmanuel <eosinnowo@getcarbon.co>
2023-01-09 16:33:45 +0100

    Updates

---
+++ minimum-difference-pair/MinimumDifferencePair.cs
@@ -0,0 +1,14 @@
+public class MinimumDifferencePair{
+    public static List<int> MinimumDifferencePair(int[] array) {
+        Array.Sort(array);
+        int minimumDifference = array[1] - array[0];
+        List<int> results = new List<int>();
+        for(int i = 2; i < array.Length; i++) {
+            minimumDifference = Math.Min(minimumDifference, array[i] - array[i - 1]);
+        }
+        for(int i = 1; i < array.Length; i++){
+            if((array[i] - array[i - 1]) == minimumDifference) { results.Add(array[i], array[i - 1]); }
+        }
+        return results;
+    }
+}
