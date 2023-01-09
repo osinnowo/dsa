4a75105928cecc2eadd01397e047c12880ae8bb8
Osinnowo Emmanuel <eosinnowo@getcarbon.co>
2023-01-09 19:09:59 +0100

    Three Number Sum -> Java, C#, Swift & Javascript

---
+++ three-sum/ThreeNumberSum.cs
@@ -0,0 +1,7 @@
+using System;
+
+public class ThreeNumberSums {
+    public static List<int[]> ThreeNumbersSum(int[] array, int targetSum) {
+        return new int[]{ };
+    }
+}
---
+++ three-sum/ThreeNumberSum.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+class ThreeNumberSum {
+    func threeNumbersSum(array: inout [Int], targetSum: Int) -> [[Int]] {
+        return []
+    }
+}
