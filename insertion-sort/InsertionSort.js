091c971404a8c3c257d473ef350a51b4abdf5724
Osinnowo Emmanuel <eosinnowo@getcarbon.co>
2023-01-19 00:04:27 +0100

    Javascript Solution

--- insertion-sort/InsertionSort.js
+++ insertion-sort/InsertionSort.js
@@ -1,4 +1,23 @@
-function insertionSort(array) {
+function insertionSort1(array) {
+    for(let i = 1; i < array.length; i++) {
+        let j = i;
+        while(j > 0 && array[j] < array[j - 1]) {
+            [array[j], array[j - 1]] = [array[j - 1], array[j]];
+            j--;
+        }
+    }
     return array;
 }
-exports.insertionSort = this.insertionSort;
+
+function insertionSort2(array) {
+    for(let i = array.length - 2; i >= 0; i--) {
+        let j = i;
+        while(j <= array.length - 2 && array[j] > array[j + 1]) {
+            [array[j], array[j + 1]] = [array[j + 1], array[j]];
+            j++;
+        }
+    }
+    return array;
+}
+
+exports.insertionSort = this.insertionSort1;
