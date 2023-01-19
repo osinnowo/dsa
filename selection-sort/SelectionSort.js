871662a1bead46b9ba52ac21645b95570b65cceb
Osinnowo Emmanuel <eosinnowo@getcarbon.co>
2023-01-19 22:19:57 +0100

    Selection Sort

---
+++ selection-sort/SelectionSort.js
@@ -0,0 +1,18 @@
+function selectionSort(array) {
+    let i = 0;
+    const length = array.length;
+    while(i < length) {
+      let smallestIndex = i;
+      let j = i + 1;
+      while(j < length){
+        if(array[smallestIndex] > array[j]) { smallestIndex = j; }
+        j++;
+      }
+      [array[smallestIndex], array[i]] = [array[i], array[smallestIndex]];
+      i++;
+    }
+    return array;
+  }
+  
+  // Do not edit the line below.
+  exports.selectionSort = selectionSort;
