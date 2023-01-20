212195890d60fd4abaf0a334f7b3434fb41a0b24
Osinnowo Emmanuel <eosinnowo@getcarbon.co>
2023-01-20 22:48:00 +0100

    Swift Solution - Bug Fixes

--- selection-sort/SelectionSort.swift
+++ selection-sort/SelectionSort.swift
@@ -17,7 +17,7 @@ class SelectionSort {
     func selectionSort2(_ array: inout [Int]) -> [Int] {
         for i in stride(from: array.count - 1, through: 0, by: -1) {
             var largestIndex = i
-            var j = i + 1
+            var j = i - 1
             while(j >= 0) {
                 if(array[largestIndex] < array[j]) { largestIndex = j }
                 j -= 1 
