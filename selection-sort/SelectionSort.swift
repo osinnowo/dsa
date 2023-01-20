2bd5a597e56ed0b1930de72e02a5f40b97aaf5aa
Osinnowo Emmanuel <eosinnowo@getcarbon.co>
2023-01-20 22:46:58 +0100

    Swift - Inverse Method

--- selection-sort/SelectionSort.swift
+++ selection-sort/SelectionSort.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 class SelectionSort {
-    func selectionSort(_ array: inout [Int]) -> [Int] {
+    func selectionSort1(_ array: inout [Int]) -> [Int] {
         for i in 0 ..< array.count {
             var j = i + 1
             var smallestIndex = i
@@ -12,5 +12,18 @@ class SelectionSort {
             array.swapAt(i, smallestIndex)
         }
         return array
+    }
+
+    func selectionSort2(_ array: inout [Int]) -> [Int] {
+        for i in stride(from: array.count - 1, through: 0, by: -1) {
+            var largestIndex = i
+            var j = i + 1
+            while(j >= 0) {
+                if(array[largestIndex] < array[j]) { largestIndex = j }
+                j -= 1 
+            }
+            (array[largestIndex], array[i]) = (array[i], array[largestIndex])
+        }
+        return array;
     }
 }
