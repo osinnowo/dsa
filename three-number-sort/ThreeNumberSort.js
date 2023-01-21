4732de3dba927a6a86c839cf160a4b53c9855ed9
Osinnowo Emmanuel <eosinnowo@getcarbon.co>
2023-01-21 16:31:05 +0100

    Javascript Solution - Inverted

--- three-number-sort/ThreeNumberSort.js
+++ three-number-sort/ThreeNumberSort.js
@@ -16,4 +16,21 @@ var threeNumberSort = (array, order) => {
     return array;
 }
 
+var threeNumberSort = (array, order) => {
+    let right = array.length - 1;
+    for(let i = order.length - 1; i > -1; i--) {
+        let left = 0;
+        while(left < right) {
+            if(array[right] === order[i]) { right--; continue; }
+            while(left < right) {
+                if(array[left] === order[i]) {
+                    [array[left], array[right]] = [array[right], array[left]];
+                    right--; break;
+                }
+                left++;
+            }
+        }
+    }
+    return array;
+}
 exports.threeNumberSort = threeNumberSort;
