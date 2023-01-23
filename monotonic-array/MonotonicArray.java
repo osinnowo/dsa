1ca7e34c8681d2673f9606109ae76f8313aed866
Osinnowo Emmanuel <eosinnowo@getcarbon.co>
2023-01-23 22:51:26 +0100

    Java Solution

--- monotonic-array/MonotonicArray.java
+++ monotonic-array/MonotonicArray.java
@@ -1,3 +1,11 @@
 public class MonotonicArray {
-    
+    public boolean isMonotonic(int[] array) {
+        if(array.length == 0) return true;
+        boolean isIncreasing = array[0] < array[array.length - 1];
+        for(int index = 0; index < array.length - 1; index++) {
+            if(isIncreasing && (array[index]) > array[index + 1]) return false;
+            if(!isIncreasing && (array[index]) < array[index + 1]) return false;
+        }
+        return true;
+    }
 }
