831b7ab7d899207501632aa1c069ac75a0a5ab00
Osinnowo Emmanuel <eosinnowo@getcarbon.co>
2023-01-23 08:38:40 +0100

    C# Solution

---
+++ reverse-linked-list/ReverseLinkedList.cs
@@ -0,0 +1,22 @@
+public class ReverseLinkedList {
+    static class LinkedList {
+        public int value;
+        public LinkedList next;
+        public LinkedList(int value, LinkedList next) {
+            this.value;
+            this.next = next;
+        }
+    }
+
+    public LinkedList ReverseLinkedLists(LinkedList head) {
+        LinkedList current = head;
+        LinkedList previous = null;
+        while(current != null) {
+            LinkedList next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+        return previous;    
+    }
+}
