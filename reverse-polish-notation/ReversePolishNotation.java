db692c4a2ed179a077db93ac7c9319d27d146656
Osinnowo Emmanuel <eosinnowo@getcarbon.co>
2023-01-29 20:24:55 +0100

    Reverse Polish Notation

---
+++ reverse-polish-notation/ReversePolishNotation.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class ReversePolishNotation {
+    //["2", "1", "+", "3", "*"]
+    public int reversePolishNotation(String[] notations) {
+        Stack<Integer> stack = new Stack<>();
+
+        int result = 0;
+        String operators = "+-*/";
+
+        for(String notation: notations) {
+            if(!operators.contains(notation)) {
+                stack.push(Integer.valueOf(notation));
+                continue;
+            }
+
+            int first = stack.pop();
+            int second = stack.pop();
+
+            int index = operators.indexOf(notation);
+            switch(index) {
+                case 0:
+                    result = first + second;
+                    break;
+                case 1:
+                    result = first - second;
+                    break;
+                case 2:
+                    result = first * second;
+                    break;
+                default:
+                    result = first / second;
+            }
+            stack.push(result);
+        }
+        return result;
+    }
+ }
