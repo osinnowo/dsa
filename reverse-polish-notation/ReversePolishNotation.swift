181004764508d643914191a686cc60e0b5585659
Osinnowo Emmanuel <eosinnowo@getcarbon.co>
2023-01-31 11:13:14 +0100

    Swift Solution

---
+++ reverse-polish-notation/ReversePolishNotation.swift
@@ -0,0 +1,32 @@
+import Foundation 
+
+class ReversePolishNotation {
+    func reversePolishNotation(_ notations: String) -> Int {
+        var result = 0;
+        var stack = [Int]()
+        var operators = "+-*/"
+        for notation in notations {
+            if !operators.contains(notation) {
+                stack.append(Int(notation)!); continue
+            }
+            var first = stack.popLast()!
+            var second = stack.popLast()!
+
+            var `operator`: Character = operators[operators.firstIndex(of: Character(notation))!]
+
+            switch `operator` {
+                case "+":
+                    result = first + second
+                case "-":
+                    result = first - second
+                case "*":
+                    result = first * second
+                default:
+                    result = first / second
+            }
+            
+            stack.append(result)
+        }
+        return result;
+    }
+}
