ee68144326f296e7c1c65ced1c509c89c7b1adb5
Osinnowo Emmanuel <eosinnowo@getcarbon.co>
2023-02-06 16:50:18 +0100

    Javascript Solution

---
+++ sum-linked-list/SumLinkedList.js
@@ -0,0 +1,29 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+      }
+}
+var sumLinkedList = (linkedListOne, linkedListTwo) => {
+    let linkedListHead = new Node(0);
+    let current = linkedListHead;
+    let carryOver = 0;
+    while(linkedListOne != null || linkedListTwo != null || carryOver != 0) {
+        let one = (linkedListOne != null) ? linkedListOne.value : 0;
+        let two = (linkedListTwo != null) ? linkedListTwo.value : 0;
+        let sum = one + two + carryOver;
+
+        let newNode = new Node(Math.floor(sum % 10));
+        current.next = newNode;
+        current = newNode;
+
+        carryOver = Math.floor(sum / 10);
+
+        linkedListOne = (linkedListOne != null) ? linkedListOne.next : null;
+        linkedListTwo = (linkedListTwo != null) ? linkedListTwo.next : null;
+    }
+    return linkedListHead.next;
+}
+
+exports.Node = Node;
+exports.sumLinkedList = sumLinkedList;
