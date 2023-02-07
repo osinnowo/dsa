a35753a3ca9bd9c78fb9b5fe219c9ce4613efc15
Osinnowo Emmanuel <eosinnowo@getcarbon.co>
2023-02-07 11:34:49 +0100

    Merge Linked List

---
+++ numeral-to-roman/NumeralToRoman.js
@@ -0,0 +1,14 @@
+const map = new Map([
+    [1, "I"],
+    [5, "V"],
+    [10, "X"],
+    [50, "L"],
+    [100, "C"],
+    [500, "D"],
+    [1000, "M"],
+])
+var numeralToRoman = (num) => {
+    
+};
+
+exports.numeralToRoman = numeralToRoman;
