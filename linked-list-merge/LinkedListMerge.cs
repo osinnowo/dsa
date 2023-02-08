cbf12a1d688b6198cb6e0cb9953cddc46212bb8d
Osinnowo Emmanuel <eosinnowo@getcarbon.co>
2023-02-08 22:21:17 +0100

    C# Solution

---
+++ linked-list-merge/LinkedListMerge.cs
@@ -0,0 +1,36 @@
+using System;
+public class LinkedListMerge {
+  public class Node {
+    int value;
+    Node next;
+
+    Node(int value) {
+      this.value = value;
+      this.next = null;
+    }
+  }
+
+  public static Node mergeLinkedLists(Node headOne, Node headTwo) {
+    Node head = new Node(0);
+    Node current = head;
+
+    while(headOne != null && headTwo != null) {
+        if(headOne.value < headTwo.value) {
+            current.next = headOne;
+            headOne = headOne.next;
+            current = current.next;
+        } else {
+            current.next = headTwo;
+            headTwo = headTwo.next;
+            current = current.next;
+        }
+    }
+    if(headOne != null) {
+        current.next = headOne;
+    }
+    if(headTwo != null) {
+        current.next = headTwo;
+    }
+    return head.next;
+  }
+}
