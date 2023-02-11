b8394db759f2e0f9c773ee76e91e5f3e2375939c
Osinnowo Emmanuel <eosinnowo@getcarbon.co>
2023-02-11 22:53:29 +0100

    Quick Sort

---
+++ quick-sort/QuickSort.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class QuickSort {
+  public static int[] quickSort(int[] array) {
+      quickSort(array, 0, array.length - 1);
+      return array;
+  }
+  
+  public static void quickSort(int[] array, int start, int end) {
+      if(start >= end) return ;
+      int left = start, right = end;
+      int pivot = array[(start + end) / 2];
+      while(left <= right) {
+          while(array[left] < pivot) {
+            left++;
+          }
+          while(array[right] > pivot) {
+            right--;
+          }
+          if(left <= right) {
+            int temp = array[right];
+            array[right] = array[left];
+            array[left] = temp;
+            left++;
+            right--;
+         }
+      }
+      quickSort(array, start, right);
+      quickSort(array, left, end);
+  }
+}
