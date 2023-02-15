704ac38d68908f9e42cd18b6f98a5dbaf8d495b4
Osinnowo Emmanuel <eosinnowo@getcarbon.co>
2023-02-15 22:36:40 +0100

    Java Solution

---
+++ max-sub-array/MaxSubArray.java
@@ -0,0 +1,11 @@
+public class MaxSubArray {
+    public int maxSubArray(int[] nums) {
+        int maxSoFar = nums[0];
+        int maxEndingHere = nums[0];
+        for(int i = 1; i < nums.length; i++) {
+            maxEndingHere = Math.max(maxEndingHere + nums[i], nums[i]);
+            maxSoFar = Math.max(maxSoFar, maxEndingHere);
+        }
+        return maxSoFar;
+    }
+}
