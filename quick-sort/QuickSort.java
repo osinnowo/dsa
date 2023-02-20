9183cdb959fb30d95983450057d8c6d7ad599aa4
Osinnowo Emmanuel <eosinnowo@getcarbon.co>
2023-02-20 12:17:54 +0100

    Justifed Comment

--- quick-sort/QuickSort.java
+++ quick-sort/QuickSort.java
@@ -1,4 +1,33 @@
-import java.util.*;
+/**
+ * QuickSort - Algorithm:
+ * This algorithm uses Lomuto Partioning Scheme, where a pivot is picked at the 
+ * middle of the input array. This algorithm is a divide & conquer implementation, 
+ * where two partitions are made from the left and right and continue to sort each 
+ * of them recursively until all the items of the array are sorted in their right 
+ * positions.                                                
+ *
+ *
+ * MIT License
+ *
+ * Copyright (c) 2023 Osinnowo Emmanuel
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of 
+ * this software and associated documentation files (the "Software"), to deal in 
+ * the Software without restriction, including without limitation the rights to 
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies 
+ * of the Software, and to permit persons to whom the Software is furnished to do 
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all 
+ * copies or substantial portions of the Software.                                
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS 
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR 
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER 
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.      
+ */
 
 class QuickSort {
   public static int[] quickSort(int[] array) {
