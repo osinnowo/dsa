57649cf71a8f65988a1d5b20856ed120a1e58ca4
Osinnowo Emmanuel <eosinnowo@getcarbon.co>
2023-03-07 22:52:03 +0100

    Java Solution

---
+++ remove-element/RemoveElement.java
@@ -0,0 +1,12 @@
+public class RemoveElement {
+    public int removeElement(int[] nums, int val) {
+         int i = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != val) {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        return i;
+    }
+ }
