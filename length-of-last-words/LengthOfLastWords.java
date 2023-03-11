bbde396367bba881cdc5faaafa71fff9b02ad014
Osinnowo Emmanuel <eosinnowo@getcarbon.co>
2023-03-11 21:10:02 +0100

    Java Solution

--- length-of-last-words/LengthOfLastWords.java
+++ length-of-last-words/LengthOfLastWords.java
@@ -1,5 +1,18 @@
-
+import java.util.*;
 
 public class LengthOfLastWords {
-    
+    public int lengthOfLastWord1(String word) { 
+        int end = word.length() - 1;
+        
+        while(end >= 0 && Character.isWhitespace(word.charAt(end))) {
+            end -= 1;
+        }
+
+        int left = end;
+        while(left >= 1 && !Character.isWhitespace(word.charAt(left))) {
+            left -= 1;
+        }
+
+        return end - left;
+    }
 }
