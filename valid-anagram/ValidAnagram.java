a724c52ed5bb2ff1ee866c18f46728cb6ca7dff1
Osinnowo Emmanuel <eosinnowo@getcarbon.co>
2023-03-22 23:25:44 +0100

    Java Solution

---
+++ valid-anagram/ValidAnagram.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class ValidAnagram {
+    public static boolean isValidAnagram(String s, String t) {
+        if(s.length() != t.length()) { return false; }
+        char[] sCharacters = s.toCharArray();
+        char[] tCharacters = t.toCharArray();
+
+        return false;
+    }
+}
