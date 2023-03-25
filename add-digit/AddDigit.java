94ee784dc4ebaaa377471e0beba7e5a1de4d5649
Osinnowo Emmanuel <eosinnowo@getcarbon.co>
2023-03-25 20:42:03 +0100

    Add Digit - Java Solution

---
+++ add-digit/AddDigit.java
@@ -0,0 +1,13 @@
+public class AddDigit {
+    public int addDigits(int num) {
+        while (num >= 10) {
+            int sum = 0;
+            while (num > 0) {
+                sum += num % 10;
+                num /= 10;
+            }
+            num = sum;
+        }
+        return num;
+    }
+}
