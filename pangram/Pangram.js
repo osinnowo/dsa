e268ceb795dbe4f699d11e61bee1cf970952172f
Osinnowo Emmanuel <eosinnowo@getcarbon.co>
2023-04-07 23:08:46 +0100

    Javascript Solution

--- pangram/Pangram.js
+++ pangram/Pangram.js
@@ -1,7 +1,9 @@
 var pangram = function(sentence) {
     let set = new Set();
     for(const character of sentence) {
-        
+        if(!set.has(character) && character.match(/[a-zA-Z]/)) {
+            set.add(character);
+        }
     }
     return set.size === 26;
 }
