bb2711ef31eb05863366d002436e63bf7804f377
Osinnowo Emmanuel <eosinnowo@getcarbon.co>
2023-04-09 10:57:22 +0100

    Method II

--- binary-search/BinarySearch.cs
+++ binary-search/BinarySearch.cs
@@ -1,5 +1,38 @@
 public class BinarySearch {
-    public int BinarySearchRecursive(int[] array, int target) {
-
+    
+    public int BinarySearchOne(int[] array, int target) {
+        return BinarySearchIterative(array, target, 0, array.Length - 1);
     }
+
+    public int BinarySearchIterative(int[] array, int target, int start, int end) {
+        while(start <= end) {
+            int middle = (start + end) / 2;
+            int potentialMatch = array[middle];
+            if(target == potentialMatch) {
+                return middle;
+            } else if (target < potentialMatch) {
+                end = middle - 1;
+            } else {
+                start = middle + 1;
+            }
+        }
+        return -1;
+    }
+
+    public int BinarySearchTwo(int[] array, int target) {
+        return BinarySearchRecursive(array, target, 0, array.Length - 1);
+    }
+
+    public int BinarySearchRecursive(int[] array, int target, int start, int end) {
+        if(start > end) { return -1; }
+        int middle = (start + end) / 2;
+        int potentialMatch = array[middle];
+        if(target == potentialMatch) {
+            return middle;
+        } else if (target < potentialMatch) {
+            return SearchRecursive(array, target, start, middle - 1);
+        } else {
+            return SearchRecursive(array, target, middle + 1, end);
+        }
+    } 
 }
