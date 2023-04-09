8c8d16e51a07adee1a8819a563d4bf34b02bee4e
Osinnowo Emmanuel <eosinnowo@getcarbon.co>
2023-04-09 10:58:39 +0100

    Java Recursive & Iterative Modification

--- binary-search/BinarySearch.java
+++ binary-search/BinarySearch.java
@@ -1,25 +1,9 @@
 public class BinarySearch {
-  public static int binarySearch(int[] array, int target) {
-    return binarySearch(array, target, 0, array.length - 1);
-  }
-  public static int binarySearch(int[] array, int target, int left, int right) {
-    if(left > right) { return - 1; }
-    int middle  = (left + right) / 2;
-    int potentialMatch = array[middle];
-    if(target == potentialMatch) {
-      return middle;
-    } else if (target < potentialMatch) {
-      return binarySearch(array, target, left, middle - 1);
-    } else {
-      return binarySearch(array, target, middle + 1, right);
-    }
+  public static int binarySearchOne(int[] array, int target) {
+    return binarySearchIterative(array, target, 0, array.length - 1);
   }
 
-  public static int binarySearchTwo(int[] array, int target) {
-    return binarySearchTwo(array, target, 0, array.length - 1);
-  }
-
-  public static int binarySearchTwo(int[] array, int target, int start, int end) {
+  public static int binarySearchIterative(int[] array, int target, int start, int end) {
     while(start <= end) {
       int middle = (start + end) / 2;
       int potentialMatch = array[middle];
@@ -32,5 +16,22 @@ public class BinarySearch {
       }
     }
     return -1;
+  }
+
+  public static int binarySearchTwo(int[] array, int target) {
+    return binarySearchRecursive(array, target, 0, array.length - 1);
+  }
+  
+  public static int binarySearchRecursive(int[] array, int target, int left, int right) {
+    if(left > right) { return - 1; }
+    int middle  = (left + right) / 2;
+    int potentialMatch = array[middle];
+    if(target == potentialMatch) {
+      return middle;
+    } else if (target < potentialMatch) {
+      return binarySearchRecursive(array, target, left, middle - 1);
+    } else {
+      return binarySearchRecursive(array, target, middle + 1, right);
+    }
   }
 }
