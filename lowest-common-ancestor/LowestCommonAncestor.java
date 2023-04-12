fc5ececbf07b242dde0f2095624b1b8cedb8a0f9
Osinnowo Emmanuel <eosinnowo@getcarbon.co>
2023-04-12 08:24:42 +0100

    Updates

--- lowest-common-ancestor/LowestCommonAncestor.java
+++ lowest-common-ancestor/LowestCommonAncestor.java
@@ -58,7 +58,8 @@
  *   
  * Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
  * Output: 2
- * Explanation: The LCA of nodes 2 and 4 is 2, since a node can be a descendant of itself according to the LCA definition. 
+ * Explanation: The LCA of nodes 2 and 4 is 2, since a node can be a 
+ * descendant of itself according to the LCA definition. 
  * Link: https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/?envType=study-plan&id=level-1
 **/ 
 
