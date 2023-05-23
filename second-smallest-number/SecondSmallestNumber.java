6e5d8161558988447692cc25a05bbe142642a8e4
Osinnowo Emmanuel <eosinnowo@getcarbon.co>
2023-05-23 23:35:22 +0100

    Java Solution

---
+++ second-smallest-number/SecondSmallestNumber.java
@@ -0,0 +1,19 @@
+package second-smallest-number;
+
+public class SecondSmallestNumber {
+    public static int findSecondSmallest(int[] nums) {
+        int smallest = Integer.MAX_VALUE;
+        int secondSmallest = Integer.MAX_VALUE;
+
+        for (int num : nums) {
+            if (num < smallest) {
+                secondSmallest = smallest;
+                smallest = num;
+            } else if (num < secondSmallest && num != smallest) {
+                secondSmallest = num;
+            }
+        }
+
+        return secondSmallest;
+    }
+}
